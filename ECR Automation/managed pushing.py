662aca97bd7867edb12efa3baffb02623feaa181
fredricklitvin <fredlitvin@gmail.com>
2025-04-27 15:16:37 +0300

    created delete and create functions

--- ECR Automation/managed pushing.py
+++ ECR Automation/managed pushing.py
@@ -1,10 +1,14 @@
 
+#used to work with aws
 import boto3
+#used to have a more interactive application
 import questionary
+from botocore.exceptions import ClientError
 
 client = boto3.client('ecr')
 iam_client = boto3.client('iam')
 
+#list all the repositories
 def list_repositories(print_repositories = False):
     i = 0
     ecr_repositories = []
@@ -19,47 +23,98 @@ def list_repositories(print_repositories = False):
         for ecr_repository in ecr_repositories:
             print(ecr_repository)
 
+#choose whether to create a new repository or delete an existing one
 def manage_repositories():
-
-    def create_repositories(repository_name = "", scan_image = False ):
-        print("hi")
     manage_menu = questionary.select(
         "Choose action:",
         choices=['Create a repository ', 'Delete a repository', 'Exit']
     ).ask()
     if manage_menu == 'Create a repository ':
-        repository_name = input("Please write your repository name: \n (The repository name must start with a letter and can only contain lowercase letters, numbers, hyphens, underscores, and forward slashes.) \n")
-
-
-
-
-
-
-
+        create_repositories()
     elif manage_menu == 'Delete a repository':
-        print("hi")
+        delete_repositories()
     else:
         return
-# chosen_repo = questionary.select(
-#     "Choose a repository:",
-#     choices=ecr_repositories
-# ).ask()
-#
-# image_tag = questionary.text("Enter the image tag (e.g. latest):").ask()
-#
-# region = questionary.select(
-#     "Choose AWS region:",
-#     choices=["us-east-1", "us-west-2"]
-# ).ask()
-#
-# print(f"You selected: {repo}, tag: {image_tag}, region: {region}")
 
+#create a new repository
+def create_repositories():
+    try:
+        #get the needed vars from the user
+        repository_name = input("Please write your repository name: \n (The repository name must start with a letter and can only contain lowercase letters, numbers, hyphens, underscores, and forward slashes.) \n")
+        scan_image = questionary.select(
+            "Please choose whether to scan the images you upload:",
+            choices=['Yes', 'No']
+        ).ask()
+        scan_image = True if scan_image == 'Yes' else False
 
+        #create the new repository
+        response_create_repository = client.create_repository(
+        repositoryName = repository_name,
+        tags = [
+            {
+                'Key': 'Owner',
+                'Value': user_name
+            }],
+            imageScanningConfiguration = {
+            'scanOnPush': scan_image})
+        print("New repository was created:",response_create_repository['repository']['repositoryName'] )
+
+    except client.exceptions.RepositoryAlreadyExistsException:
+        print(" Error: A repository with that name already exists.")
+
+    except client.exceptions.InvalidParameterException:
+        print(" Error: Invalid parameters provided. Please double-check the repository name or other settings.")
+
+    except ClientError as e:
+        print(f" ClientError: {e.response['Error']['Message']}")
+
+    except Exception as e:
+        print(f" An unexpected error occurred: {str(e)}")
+
+#delete a repository
+def delete_repositories():
+    try:
+        repositories_list = list_repositories(print_repositories = False)
+        repository_deletion = questionary.select(
+            "Choose a repository for deletion:",
+            choices=repositories_list
+        ).ask()
+        response_deleted_repository = client.delete_repository(
+            repositoryName=repository_deletion,
+            force=False
+        )
+        print("Repository was deleted successfully: ",response_deleted_repository['repository']['repositoryName'])
+
+    except client.exceptions.RepositoryNotEmptyException:
+        force_delete = questionary.select(
+            "Repository is not empty, do you want to force delete?:",
+            choices=['Yes', 'No']
+        ).ask()
+        scan_image = True if force_delete == 'Yes' else False
+        if scan_image:
+            response_deleted_repository = client.delete_repository(
+                repositoryName=repository_deletion,
+                force=True
+            )
+        else:
+            print("No repository was deleted because it is not empty")
+
+    except ClientError as e:
+        print(f" ClientError: {e.response['Error']['Message']}")
+
+    except Exception as e:
+        print(f" An unexpected error occurred: {str(e)}")
+
+#makes sure the program doesn't stop until being told
 exit_menu = False
-while not exit_menu:
-    response_user = iam_client.get_user()
-    print(response_user)
 
+# get the username for tagging
+response_user = iam_client.get_user()
+user_name_split = response_user['User']['Arn'].split(':')
+user_name = user_name_split[-1]
+
+# the main menu where you decide what action to use
+while not exit_menu:
     starting_menu = questionary.select(
         "Choose action:",
         choices=['List repositories','Manage repositories','Upload images','Exit']
